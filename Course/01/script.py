1ee90390c854e0c62c0d4018ab40756a9fdf09df
Alberto <betim009@gmail.com>
2023-05-23 15:15:48 -0300

    feat: primeiros passos.

---
+++ Course/01/script.py
@@ -0,0 +1,38 @@
+# Saída:
+print("Olá mundo!")
+
+# Operações Básicas:
+print(2 * 2)
+print(4 / 2)
+print(2 + 2)
+print(2 - 2)
+
+# Atribuir valor a uma variavél:
+nome = "Alberto"
+idade = 27
+flamenguista = True
+
+# Incremento:
+counter = 0
+counter += 1  # ou counter = counter + 1
+
+# Decremento
+counter_2 = 1
+counter_2 -= 1
+
+# Tipos de saídas:
+3 // 2  # saída: 1
+
+3 / 2  # saída: 1.5
+
+# Verificando tipos
+print("Verificando tipos:")
+print("1" == 1)
+print(1 == 1)
+print(2 > 1)
+
+# Operadores And e OR:
+print("Operadores And e Or:")
+temperatura = 45
+print(temperatura > 10 and temperatura < 40)
+print(temperatura > 10 or temperatura < 40)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Python-Course
-Guia de Python
+### Primeiros passos:
+    * Instalar python.
+    * Verificar a versão: -python3 --version -.
+    * Para abrir o terminal do python: -python3-.
+    * Instalar a extensão no vscode: Code Runer.
+    * Exemplo para rodar um arquivo: -python3 Course/01/script.py-.
