11d3e2c821fc4f8b8c6c7ef722eaa6bb06c81dc7
Alberto <betim009@gmail.com>
2023-05-23 21:32:24 -0300

    feat: script1.py

---
+++ Course/01/script1.py
@@ -0,0 +1,32 @@
+# Exercício 1:
+# No terminal, inicialize duas variáveis a e b, sendo a = 10 e b = 5.
+# Mostre o resultado das 7 operações básicas (soma, subtração, multiplicação,
+# divisão, divisão inteira, potenciação e módulo) envolvendo essas variáveis.
+
+a = 10
+b = 5
+
+soma = a + b
+sub = a - b
+mult = a * b
+div = a / b
+divInt = a // b
+pot = a**b
+mod = a % b
+
+print("Ex.1:")
+print(soma, sub, mult, div, divInt, pot, mod)
+
+
+# Exercício 2:
+# Declare e inicialize uma variável: hours = 6.
+# Quantos minutos têm em 6 horas?
+# E quantos segundos? Declare e inicialize variáveis minutes e seconds
+# que recebem os respectivos resultados das contas. Depois,
+# imprima cada uma delas.
+
+hours = 6
+minutes = hours * 60
+seconds = minutes * 60
+
+print(hours, minutes, seconds)
