9caf30dc13659db4d16bfa87a89f1b6aebec16c5
Alberto <betim009@gmail.com>
2023-07-05 16:51:26 -0300

    scrip10.funcoes.py

--- Course/01/projeto-soma/defSoma.py
+++ Course/01/projeto-soma/defSoma.py
@@ -1,6 +1,7 @@
-def soma(n, n2):
-    if (type(n) == int and type(n2) == int):
-        r = n + n2
+def soma(n1, n2):
+    # validacoes se nums são inteiros
+    if (type(n1) == int and type(n2) == int):
+        r = n1 + n2
         print(f"Resultado: {r}")
     else:
         print("Insira dados com o formato int!")
---
+++ Course/01/script10.funcoes.py
@@ -0,0 +1,16 @@
+# 1. criando 1 função:
+def soma(numero1, numero2):
+    return numero1 + numero2
+
+
+soma_1 = soma(10, 10)
+print(soma_1)
+
+
+# 2. paramentro * para separar string
+def separar(string):
+    print(*string)
+
+
+# separar1 = separar('beto')
+# separar1
